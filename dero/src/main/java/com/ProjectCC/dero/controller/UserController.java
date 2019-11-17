9a8a8912755af5473a75cec3a4bdd70a08740148
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-11-17 14:48:52 +0100

    Implemented adding new user

--- dero/src/main/java/com/ProjectCC/dero/controller/UserController.java
+++ dero/src/main/java/com/ProjectCC/dero/controller/UserController.java
@@ -1,15 +1,42 @@
 package com.ProjectCC.dero.controller;
 
 
+import com.ProjectCC.dero.model.User;
 import com.ProjectCC.dero.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resources;
+
+
+@RestController
+@RequestMapping(value = "api/users")
 public class UserController {
 
     private UserService userService;
 
     @Autowired
-    public void setUserService(UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        User newUser = user;
+
+        newUser = userService.save(newUser);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+
+    @GetMapping(value = "{email}")
+    public ResponseEntity<User> getUser(@PathVariable String email) {
+        User user = new User();
+
+        user = userService.findByEmail(email);
+        return new ResponseEntity<>(user, HttpStatus.FOUND);
+    }
+
 }
---
+++ dero/src/main/java/com/ProjectCC/dero/dto/UserDTO.java
@@ -0,0 +1,15 @@
+package com.ProjectCC.dero.dto;
+
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String jmbg;
+    private String password;
+    private String email;
+    private String address;
+    private String city;
+    private String country;
+    private String telephone;
+
+}
