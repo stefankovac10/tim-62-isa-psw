501120c1e8f31163758c88555173af239fa7f6fa
meelosch <57445151+meelosch@users.noreply.github.com>
2019-11-20 19:12:27 +0100

    Implemented back-end for registration

--- dero/src/main/java/com/ProjectCC/dero/model/RegistrationRequest.java
+++ dero/src/main/java/com/ProjectCC/dero/model/RegistrationRequest.java
@@ -8,17 +8,36 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-public class RegistrationRequest {
+public class RegistrationRequest extends User{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne()
+    /*@OneToOne()
     @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private User user;
+    private User user;*/
 
     private boolean verified;
 
     public RegistrationRequest() {
+        super();
+    }
+
+    public RegistrationRequest(String firstName, String lastName, String jmbg, String password, String email, String address, String city, String country, String telephone, boolean verified) {
+        super(firstName, lastName, jmbg, password, email, address, city, country, telephone);
+        this.verified = verified;
+    }
+
+    public RegistrationRequest(User user, boolean verified) {
+        super(user.getFirstName(),
+                user.getLastName(),
+                user.getJmbg(),
+                user.getPassword(),
+                user.getEmail(),
+                user.getAddress(),
+                user.getCity(),
+                user.getCountry(),
+                user.getTelephone());
+        this.verified = verified;
     }
 }
