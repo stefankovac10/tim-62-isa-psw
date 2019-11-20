c72320cd2d5036e95852801e811ae83980cae9d3
meelosch <57445151+meelosch@users.noreply.github.com>
2019-11-20 01:32:48 +0100

    Initial look of patient's homepage
    Created navigation bar and vue components nedded for initial look of patient's homepage.

---
+++ clinic-center-project/src/views/patient/PClinics.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="d-flex p-2 justify-content-center">
+    <h1>List of clinics</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {};
+  }
+};
+</script>
