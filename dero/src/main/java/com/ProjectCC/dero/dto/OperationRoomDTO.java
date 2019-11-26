9fa5399e95ff431cc90bff1602ef6e77998c422b
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-11-26 09:28:25 +0100

    Implemented back-end logic for retrieving and deleteing operating rooms

--- dero/src/main/java/com/ProjectCC/dero/dto/OperationRoomDTO.java
+++ dero/src/main/java/com/ProjectCC/dero/dto/OperationRoomDTO.java
@@ -1,18 +1,23 @@
 package com.ProjectCC.dero.dto;
 
+import com.ProjectCC.dero.model.Operation;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 
 @Getter
-@Setter
 public class OperationRoomDTO {
+    @Setter
     private Long id;
     private int number;
     private String name;
 
     @Autowired
-    public OperationRoomDTO(int number, String name) {
+    public OperationRoomDTO() {
+    }
+
+    public OperationRoomDTO(Long id, int number, String name) {
+        this.id = id;
         this.number = number;
         this.name = name;
     }
--- dero/src/main/java/com/ProjectCC/dero/service/OperationRoomService.java
+++ dero/src/main/java/com/ProjectCC/dero/service/OperationRoomService.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class OperationRoomService {
     private OperationRoomRepository operationRoomRepository;
@@ -24,5 +27,22 @@ public class OperationRoomService {
         operationRoom.setId(or.getId());
 
         return new ResponseEntity<>(operationRoom, HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<OperationRoomDTO>> getAll() {
+        List<OperationRoom> list = this.operationRoomRepository.findAll();
+        List<OperationRoomDTO> ret = new ArrayList<>();
+
+        for (OperationRoom or : list) {
+            ret.add(new OperationRoomDTO(or.getId(), or.getNumber(), or.getName()));
+        }
+
+        return new ResponseEntity<>(ret, HttpStatus.FOUND);
+    }
+
+    public ResponseEntity<Void> delete(Long id) {
+        this.operationRoomRepository.deleteById(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
