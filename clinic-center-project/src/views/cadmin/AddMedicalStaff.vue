8a6c49e1971bc130cea6d7d7638c7147e1473f91
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-02 20:15:58 +0100

    Vue component for adding medical staff

---
+++ clinic-center-project/src/views/cadmin/AddMedicalStaff.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <RegisterForm v-bind:doc="true" v-on:register="registerDoc(arguments)"></RegisterForm>
+  </div>
+</template>
+
+<script>
+import { httpClient } from "@/services/Api.js";
+import RegisterForm from "@/components/RegisterForm.vue";
+
+export default {
+  components: {
+    RegisterForm
+  },
+  data: function() {
+    return {
+      password: undefined,
+      name: undefined,
+      surname: undefined,
+      jmbg: undefined,
+      telephone: undefined,
+      country: undefined,
+      city: undefined,
+      address: undefined,
+      email: undefined,
+      clinic: undefined
+    };
+  },
+  methods: {
+    registerDoc: function(args) {
+      if (args[1] === "Nurse") {
+        httpClient
+          .post("/users/nurse", args[0])
+          .then(function(response) {
+            alert(response.data.email);
+          })
+          .catch(function(error) {
+            alert(error.response);
+          });
+        this.$router.push("/cadmin");
+
+        return;
+      }
+
+      httpClient
+        .post("/users/doc", args[0])
+        .then(function(response) {
+          alert(response.data.email);
+        })
+        .catch(function(error) {
+          alert(error.response);
+        });
+      this.$router.push("/cadmin");
+    }
+  }
+};
+</script>
