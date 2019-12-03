272eb8971d271bc8562c28fb21c55f9b6b3c43df
stefankovac10 <stefankovac42@gmail.com>
2019-12-03 14:43:50 +0100

    Connected front-end and backend for getting all prescription and certifying prescription.

--- clinic-center-project/src/views/nurse/Perscription.vue
+++ clinic-center-project/src/views/nurse/Perscription.vue
@@ -1,26 +1,48 @@
 <template>
   <div class="d-flex flex-row flex-wrap p-2 justify-content-center">
-      <div class="card border-success mb-3" style="max-width: 20rem; max-height: 15rem; height:15rem; width:20rem; margin-right: 10px; margin-top:20px">
-        <div class="card-body">
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <button type="button" style="position: absolute; right: 0; bottom:0; margin: 10px" class="btn btn-success" v-on:click="certify">Certify</button>
+      <div class="card border-success mb-3" style="max-width: 20rem; max-height: 15rem; height:15rem; width:20rem; margin-right: 10px; margin-top:20px"
+       v-for = "prescription in prescriptions" v-bind:key="prescription">
+        <div v-if="prescription.certified === false">
+          <div>Doctor: {{prescription.doctor.firstName}} {{prescription.doctor.lastName}}</div>
+          <div class="card-body">
+            <p class="card-text" v-for = "medication in prescription.medication" v-bind:key = "medication">{{medication.name}}</p>
+            <button type="button" style="position: absolute; right: 0; bottom:0; margin: 10px" class="btn btn-success" v-on:click="certify(prescription.id)">Certify</button>
+          </div>
         </div>
       </div>
   </div>
 </template>
 
 <script>
+import { httpClient } from "@/services/Api.js";
 export default {
   name: "perscription",
   data: function() {
-    return {};
+    return {
+      prescriptions: {},
+      prescription:{}
+    };
   }, 
   mounted(){
-
+      httpClient
+        .get("/prescription/all")
+        .then(response => {
+          this.prescriptions = response.data;      
+        })
+        .catch(error => {
+          this.error = error;
+        });
   },
   methods:{
-      certify: function(){
-
+      certify: function(id){
+          httpClient
+        .get("/prescription/certify/"+id)
+        .then(response => {
+          this.prescription = response.data;      
+        })
+        .catch(error => {
+          this.error = error;
+        });
       }
   }
 };
