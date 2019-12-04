349a01312fad6cc98d2d9c6b7e7f92e32006bc4e
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-04 17:50:06 +0100

    Implemented back-end functions for adding new request, changed Clinic and Vacation request classes to reference each other

--- dero/src/main/java/com/ProjectCC/dero/model/VacationRequest.java
+++ dero/src/main/java/com/ProjectCC/dero/model/VacationRequest.java
@@ -1,23 +1,28 @@
 package com.ProjectCC.dero.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
+@Builder
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class VacationRequest {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
    @Column(name = "date", nullable = false)
-   private String date;
+   private String startDate;
 
    @Column(name = "duration", nullable = false)
-   private String duration;
+   private String endDate;
+
+   @Column(name = "accepted")
+   private boolean accepted;
 
    @OneToOne(mappedBy = "vacationRequest")
    public MedicalStaff medicalStaff;
@@ -26,6 +31,9 @@ public class VacationRequest {
    @JoinColumn(name = "cadmin_id", nullable = false)
    private ClinicAdministrator administrator;
 
-   public VacationRequest() {
-   }
+   @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+   private Clinic clinic;
+
+//   public VacationRequest() {}
+
 }
