986b985f85836155d13c412b9a0f892898f9afc9
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-11 20:19:19 +0100

    Added builder to patient dto and getters/setters

--- dero/src/main/java/com/ProjectCC/dero/dto/UserDTO.java
+++ dero/src/main/java/com/ProjectCC/dero/dto/UserDTO.java
@@ -24,42 +24,4 @@ public class UserDTO {
     private String city;
     private String country;
     private String telephone;
-
-//    public UserDTO() {
-//    }
-//
-//    public UserDTO(User user) {
-//        this(user.getId(), user.getFirstName(), user.getLastName(), user.getJmbg(),
-//                user.getPassword(), user.getEmail(), user.getAddress(), user.getCity(),
-//                user.getCountry(), user.getTelephone());
-//    }
-//
-//    public UserDTO(String firstName, String lastName, String jmbg,
-//                   String password, String email, String address, String city,
-//                   String country, String telephone) {
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.jmbg = jmbg;
-//        this.password = password;
-//        this.email = email;
-//        this.address = address;
-//        this.city = city;
-//        this.country = country;
-//        this.telephone = telephone;
-//    }
-//
-//    public UserDTO(Long id, String firstName, String lastName, String jmbg,
-//                   String password, String email, String address, String city,
-//                   String country, String telephone) {
-//        this.id = id;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.jmbg = jmbg;
-//        this.password = password;
-//        this.email = email;
-//        this.address = address;
-//        this.city = city;
-//        this.country = country;
-//        this.telephone = telephone;
-//    }
 }
