0262d9e91e9c3b72d7a0992189690ac665faf27c
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-11 20:19:57 +0100

    Implemented functions that searches patients in database by given params

--- dero/src/main/java/com/ProjectCC/dero/repository/PatientRepository.java
+++ dero/src/main/java/com/ProjectCC/dero/repository/PatientRepository.java
@@ -2,8 +2,15 @@ package com.ProjectCC.dero.repository;
 
 import com.ProjectCC.dero.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByFirstNameAndLastNameAndJmbgAllIgnoringCase(String firstName, String lastName, String jmbg);
+
+    @Query("select p from Patient p where lower(p.firstName) like lower(?1) and lower(p.lastName) like lower(?2) and lower(p.jmbg) like lower(?3)")
+    List<Patient> search(String firstName, String lastName, String jmbg);
 }
