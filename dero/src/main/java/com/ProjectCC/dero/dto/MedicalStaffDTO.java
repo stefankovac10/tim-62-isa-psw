89d6b317feaba497f0c063657411c96e29c47bd8
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-13 16:07:37 +0100

    Added specialsed type to doctor back-end

--- dero/src/main/java/com/ProjectCC/dero/dto/MedicalStaffDTO.java
+++ dero/src/main/java/com/ProjectCC/dero/dto/MedicalStaffDTO.java
@@ -1,6 +1,7 @@
 package com.ProjectCC.dero.dto;
 
 import com.ProjectCC.dero.model.Clinic;
+import com.ProjectCC.dero.model.MedicalRecord;
 import com.ProjectCC.dero.model.VacationRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,16 +10,17 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+import java.util.Set;
+
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 public class MedicalStaffDTO extends UserDTO{
-
-    @Getter
+    private Long id;
     public ClinicDTO clinic;
-
     private VacationRequestDTO vacationRequest;
-
+    public List<MedicalRecord> medicalRecords;
 //    @Autowired
 //    public MedicalStaffDTO() {
 //    }
