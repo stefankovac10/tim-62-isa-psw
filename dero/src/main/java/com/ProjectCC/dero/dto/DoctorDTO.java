72b99196ee691f6dd70683a888063728f835d5ad
stefankovac10 <stefankovac42@gmail.com>
2019-12-13 15:22:43 +0100

    Added builder pattern for DTO and removed uneccessary constructor

--- dero/src/main/java/com/ProjectCC/dero/dto/DoctorDTO.java
+++ dero/src/main/java/com/ProjectCC/dero/dto/DoctorDTO.java
@@ -24,30 +24,4 @@ public class DoctorDTO extends MedicalStaffDTO {
     public Set<OperationDTO> operations;
     private TypeOfExaminationDTO specialisedType;
 
-//    @Autowired
-//    public DoctorDTO() {
-//        super();
-//    }
-//
-//    public DoctorDTO(String firstName, String lastName, String jmbg, String password,
-//                       String email, String address, String city, String country,
-//                       String telephone) {
-//        super(firstName, lastName, jmbg, password, email, address, city, country,
-//                telephone);
-//    }
-//
-//    @Autowired
-//    public DoctorDTO(Long id, String firstName, String lastName, String jmbg, String password,
-//                     String email, String address, String city, String country,
-//                     String telephone, ClinicDTO clinic) {
-//        super(id, firstName, lastName, jmbg, password, email, address, city, country,
-//                telephone, clinic);
-//    }
-//
-//    @Autowired
-//    public DoctorDTO(Doctor doctor) {
-//        super(doctor.getId(), doctor.getFirstName(), doctor.getLastName(), doctor.getJmbg(),
-//                doctor.getPassword(), doctor.getEmail(), doctor.getAddress(),
-//                doctor.getCity(), doctor.getCountry(), doctor.getTelephone(), new ClinicDTO(doctor.getClinic()));
-//    }
 }
--- dero/src/main/java/com/ProjectCC/dero/dto/RegistrationRequestDTO.java
+++ dero/src/main/java/com/ProjectCC/dero/dto/RegistrationRequestDTO.java
@@ -1,11 +1,17 @@
 package com.ProjectCC.dero.dto;
 
 import com.ProjectCC.dero.model.RegistrationRequest;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+@SuperBuilder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegistrationRequestDTO {
     private Long id;
     private String firstName;
@@ -19,40 +25,4 @@ public class RegistrationRequestDTO {
     private String telephone;
     private boolean verified;
 
-    public RegistrationRequestDTO() {
-    }
-
-    public RegistrationRequestDTO(String firstName, String lastName, String jmbg, String password, String email, String address, String city, String country, String telephone, boolean verified) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.jmbg = jmbg;
-        this.password = password;
-        this.email = email;
-        this.address = address;
-        this.city = city;
-        this.country = country;
-        this.telephone = telephone;
-        this.verified = verified;
-    }
-
-    public RegistrationRequestDTO(Long id, String firstName, String lastName, String jmbg, String password, String email, String address, String city, String country, String telephone, boolean verified) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.jmbg = jmbg;
-        this.password = password;
-        this.email = email;
-        this.address = address;
-        this.city = city;
-        this.country = country;
-        this.telephone = telephone;
-        this.verified = verified;
-    }
-
-    public RegistrationRequestDTO(RegistrationRequest registrationRequest){
-        this(registrationRequest.getId(),registrationRequest.getFirstName(),registrationRequest.getLastName(),
-               registrationRequest.getJmbg(), registrationRequest.getPassword(), registrationRequest.getEmail(),
-                registrationRequest.getAddress(), registrationRequest.getCountry(), registrationRequest.getCountry(),
-                registrationRequest.getTelephone(), registrationRequest.isVerified());
-    }
 }
