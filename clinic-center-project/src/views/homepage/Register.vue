15a9579036bbc57c523d046c3170684fc6767668
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-14 19:20:53 +0100

    Changed route for back-end

--- clinic-center-project/src/views/homepage/Register.vue
+++ clinic-center-project/src/views/homepage/Register.vue
@@ -38,30 +38,47 @@ export default {
   },
   methods: {
     registerUser: function(event) {
-      
-        if(event.firstName === ''  || event.firstName === undefined || event.lastName === ''  || event.lastName === undefined || event.password === ''  || event.password === undefined
-          || event.email === ''  || event.email === undefined || event.jmbg === ''  || event.jmbg === undefined || event.telephone === ''  || event.telephone === undefined
-          || event.country === ''  || event.country === undefined || event.city === ''  || event.city === undefined || event.address === ''  || event.address === undefined){
-              this.$vToastify.info({
-              body: "Please, fill all the information",
-              title: "Info",
-              type: "info",
-              canTimeout: true,
-              append: false
-            });
-              return;
-        }
-        httpClient
-        .post("/regrequest", event)
+      if (
+        event.firstName === "" ||
+        event.firstName === undefined ||
+        event.lastName === "" ||
+        event.lastName === undefined ||
+        event.password === "" ||
+        event.password === undefined ||
+        event.email === "" ||
+        event.email === undefined ||
+        event.jmbg === "" ||
+        event.jmbg === undefined ||
+        event.telephone === "" ||
+        event.telephone === undefined ||
+        event.country === "" ||
+        event.country === undefined ||
+        event.city === "" ||
+        event.city === undefined ||
+        event.address === "" ||
+        event.address === undefined
+      ) {
+        this.$vToastify.info({
+          body: "Please, fill all the information",
+          title: "Info",
+          type: "info",
+          canTimeout: true,
+          append: false
+        });
+        return;
+      }
+      httpClient
+        .post("/regrequest/new", event)
         .then(response => {
           this.response = response;
           this.$vToastify.info({
-              body: "Your registration request have been sent, please wait email with information",
-              title: "Success",
-              type: "success",
-              canTimeout: true,
-              append: false
-            });
+            body:
+              "Your registration request have been sent, please wait email with information",
+            title: "Success",
+            type: "success",
+            canTimeout: true,
+            append: false
+          });
         })
         .catch(error => {
           this.error = error;
