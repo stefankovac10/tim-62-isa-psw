54712c8f732bb98ed4d34f3b1785f72b76cfc87f
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2019-12-15 14:47:00 +0100

    Refreshing tokens on front-end

--- clinic-center-project/src/services/Api.js
+++ clinic-center-project/src/services/Api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import moment from 'moment';
 
 let AUTH_TOKEN = "Bearer " + localStorage.getItem("User-token");
+let authTokenRequest = "Bearer " + localStorage.getItem("User-token");
 let config = {
 	baseURL: "http://localhost:8080/api",
 	headers: {
@@ -9,27 +11,75 @@ let config = {
 };
 
 const httpClient = axios.create(config);
-httpClient.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// httpClient.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
-// Token interceptor
-// const authInterceptor = config => {
-//     return config;
-// }
+function resetAuthTokenRequest() {
+	authTokenRequest = null
+}
 
-// const loggerInterceptor = config => {
-//     return config;
-// }
+function logout() {
+	localStorage.removeItem("User-token");
+	localStorage.removeItem("Expiary");
+	localStorage.removeItem("Email");
+	localStorage.removeItem("Authority");
+}
+
+function getAuthToken() {
+	// if the current store token expires soon
+	if (moment().subtract(40, 'second').isBefore(localStorage.getItem("Expires"))) {
+		// if not already requesting a token
+		if (authTokenRequest === null) {
+			authTokenRequest = axios.post('/auth/refresh', {}, { withCredentials: true })
+				.then(response => {
+					// request complete and store
+					resetAuthTokenRequest()
+					localStorage.setItem("User-token", response.data.accessToken);
+					localStorage.setItem("Email", response.data.email);
+					localStorage.setItem("Authority", response.data.authority);
+
+					let time = moment().add(response.data.expiresIn);
+					localStorage.setItem("Expiary", time);
+					return response.data.access_token
+				})
+		}
+		return authTokenRequest
+	}
+	return localStorage.getItem("User-token");
+}
+
+axios.interceptors.request.use(function (config) {
+	// Do something before request is sent
+	if (!config.url.includes('login') && !config.url.includes('refresh') && !config.url.includes('reqrequest') && !config.url.includes('reset_password') && !config.url.includes('activate')) {
+		config.headers['Authorization'] = 'Bearer ' + getAuthToken();
+	} else {
+		config.headers['Authorization'] = 'Bearer ' + AUTH_TOKEN;
+	}
+	return config;
+}, function (error) {
+	// Do something with request error
+	return Promise.reject(error);
+});
 
 /** Adding the response interceptors */
-httpClient.interceptors.response.use(
-	response => {
-		/** TODO: Add any response interceptors */
-		return response;
-	},
-	error => {
-		/** TODO: Do something with response error */
-		return Promise.reject(error);
+httpClient.interceptors.response.use(function (config) {
+	return config;
+}, function (error) {
+	// Prevent endless redirects (login is where you should end up)
+	if (error.request !== undefined) {
+		if (error.request.responseURL.includes('login')) {
+			return Promise.reject(error)
+		}
 	}
-);
+
+	// If you can't refresh your token or you are sent Unauthorized on any request, logout and go to login
+	if (error.request !== undefined && (error.request.responseURL.includes('refresh') || error.request.status === 401 && error.config.__isRetryRequest)) {
+		logout();
+		this.$router.push({ name: 'Login' })
+	} else if (error.request !== undefined && error.request.status === 401) {
+		error.config.__isRetryRequest = true
+		return axios.request(error.config)
+	}
+	return Promise.reject(error)
+});
 
 export { httpClient };
