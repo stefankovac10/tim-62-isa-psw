6ecf67fbc21c1235afa8ffbc1e13b848c20cb155
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-01-24 16:16:25 +0100

    Created back-end logic for scheduling next examination
    Added controller and servce classes that implement functions for scheduling next examination.

---
+++ dero/src/main/java/com/ProjectCC/dero/model/ExaminationRequest.java
@@ -0,0 +1,41 @@
+package com.ProjectCC.dero.model;
+
+import lombok.*;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExaminationRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long doctorId;
+
+    @Column
+    private Long patientId;
+
+    @Column
+    private Long typeId;
+
+    @Column(name = "date", nullable = false)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime", parameters = {
+            @org.hibernate.annotations.Parameter(name = "databaseZone", value = "UTC"),
+            @org.hibernate.annotations.Parameter(name = "javaZone", value = "UTC")
+    })
+    private DateTime date;
+
+    @Column(name = "duration", nullable = false)
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDurationAsMillisLong")
+    private Duration duration;
+
+}
