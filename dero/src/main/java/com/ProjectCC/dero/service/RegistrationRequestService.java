cec83e52555119bb63e9000716f6f61dec2b70f1
stefankovac10 <stefankovac42@gmail.com>
2020-01-31 22:55:52 +0100

    Added geting list of clinics/diagnosis/medicines/request by page

--- dero/src/main/java/com/ProjectCC/dero/service/RegistrationRequestService.java
+++ dero/src/main/java/com/ProjectCC/dero/service/RegistrationRequestService.java
@@ -1,13 +1,19 @@
 package com.ProjectCC.dero.service;
 
+import com.ProjectCC.dero.dto.RegistrationRequestDTO;
 import com.ProjectCC.dero.dto.UserDTO;
 import com.ProjectCC.dero.model.Authority;
 import com.ProjectCC.dero.model.RegistrationRequest;
 import com.ProjectCC.dero.repository.RegistrationRequestRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,12 +22,14 @@ public class RegistrationRequestService {
     private RegistrationRequestRepository registrationRequestRepository;
     private PasswordEncoder passwordEncoder;
     private AuthorityService authorityService;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public RegistrationRequestService(RegistrationRequestRepository registrationRequestRepository, PasswordEncoder passwordEncoder, AuthorityService authorityService) {
+    public RegistrationRequestService(RegistrationRequestRepository registrationRequestRepository, ModelMapper modelMapper, PasswordEncoder passwordEncoder, AuthorityService authorityService) {
         this.registrationRequestRepository = registrationRequestRepository;
         this.passwordEncoder = passwordEncoder;
         this.authorityService = authorityService;
+        this.modelMapper = modelMapper;
     }
 
     public RegistrationRequest save(RegistrationRequest registrationRequest){
@@ -33,8 +41,29 @@ public class RegistrationRequestService {
         return registrationRequestRepository.save(registrationRequest);
     }
 
-    public List<RegistrationRequest> findAll() {
-        return registrationRequestRepository.findAll();
+    public List<RegistrationRequestDTO> findAll(int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        Page<RegistrationRequest> registrationRequests = registrationRequestRepository.findAll(pageable);
+
+        List<RegistrationRequestDTO> registrationRequestDTOS = new ArrayList<>();
+        for(RegistrationRequest r : registrationRequests){
+            registrationRequestDTOS.add(RegistrationRequestDTO.builder()
+                                        .firstName(r.getFirstName())
+                                        .lastName(r.getLastName())
+                                        .telephone(r.getTelephone())
+                                        .address(r.getAddress())
+                                        .city(r.getCity())
+                                        .country(r.getCountry())
+                                        .email(r.getEmail())
+                                        .id(r.getId())
+                                        .jmbg(r.getJmbg())
+                                        .password(r.getPassword())
+                                        .verified(r.isVerified())
+                                        .pages(registrationRequests.getTotalPages())
+                                        .build());
+        }
+
+        return registrationRequestDTOS;
     }
 
     public RegistrationRequest findById(Long id) {
