21c5cd8998618244b67bb0c68da5e9a2a0b846bf
stefankovac10 <stefankovac42@gmail.com>
2020-02-04 12:14:37 +0100

    Added controller for operation request and implemented logic for getting all requests

--- dero/src/main/java/com/ProjectCC/dero/service/OperationRequestService.java
+++ dero/src/main/java/com/ProjectCC/dero/service/OperationRequestService.java
@@ -1,35 +1,37 @@
 package com.ProjectCC.dero.service;
 
 import com.ProjectCC.dero.dto.OperationRequestDTO;
-import com.ProjectCC.dero.model.Doctor;
-import com.ProjectCC.dero.model.OperationRequest;
-import com.ProjectCC.dero.model.Patient;
+import com.ProjectCC.dero.model.*;
 import com.ProjectCC.dero.repository.DoctorRepository;
 import com.ProjectCC.dero.repository.OperationRequestRepository;
+import com.ProjectCC.dero.repository.UserRepository;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.joda.time.Duration;
 import org.joda.time.Period;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 
-import java.util.HashSet;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 @Service
 public class OperationRequestService {
 
     private OperationRequestRepository operationRequestRepository;
+    private UserRepository userRepository;
     private ModelMapper modelMapper;
 
     @Autowired
-    public OperationRequestService(OperationRequestRepository operationRequestRepository, ModelMapper modelMapper) {
+    public OperationRequestService(UserRepository userRepository, OperationRequestRepository operationRequestRepository, ModelMapper modelMapper) {
         this.operationRequestRepository = operationRequestRepository;
         this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
     }
 
     public ResponseEntity<Void> save(OperationRequestDTO operationRequestDTO) {
@@ -39,4 +41,36 @@ public class OperationRequestService {
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    public List<OperationRequestDTO> findOperations(String email,int page) {
+        Pageable pageable = PageRequest.of(page, 10);
+        ClinicAdministrator user = (ClinicAdministrator)userRepository.findByEmail(email);
+
+        Page<OperationRequest> operations = operationRequestRepository.findByClinic(user.getClinic().getId(),pageable);
+        List<OperationRequestDTO> operationRequestDTOS = new ArrayList<>();
+
+        if(operations == null){
+            return null;
+        }else{
+            for(OperationRequest o: operations){
+                User patient = userRepository.getOne(o.getPatientId());
+                User doctor = userRepository.getOne(o.getDoctorId());
+                OperationRequestDTO or= OperationRequestDTO.builder()
+                                        .date(o.getDate())
+                                        .doctorId(o.getDoctorId())
+                                        .id(o.getId())
+                                        .duration(o.getDuration())
+                                        .patientId(o.getPatientId())
+                                        .patientName(patient.getFirstName()+" "+ patient.getLastName())
+                                        .doctorName(doctor.getFirstName()+" "+doctor.getLastName())
+                                        .pages(operations.getTotalPages())
+                                        .build();
+
+                operationRequestDTOS.add(or);
+            }
+            return operationRequestDTOS;
+        }
+
+    }
+
 }
