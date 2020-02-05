7c8d097c8bd9950a52e5d749a33642be32960c55
meelosch <57445151+meelosch@users.noreply.github.com>
2020-02-06 00:50:31 +0100

    replacing old Login.vue with Login.vue from master
    with a purpose of solving conflicts

--- clinic-center-project/src/views/homepage/Login.vue
+++ clinic-center-project/src/views/homepage/Login.vue
@@ -3,18 +3,10 @@
     <form accept-charset="UTF-8" class="d-flex flex-column col-sm-4">
       <h1 class="p-2">Log in</h1>
 
-      <div class="d-flex justify-content-between">
-        <label class="p-2">E-mail</label>
-        <label for="text" class="p-2" id="err">{{emailMessage}}</label>
-      </div>
+      <label class="p-2">E-mail</label>
       <input type="text" class="p-2" id="email" v-model="email" />
-      
-      <div class="d-flex justify-content-between">
-        <label class="p-2" for="password">Password</label>
-        <label class="p-2" id="err">{{passwordMessage}}</label>
-      </div>
+      <label class="p-2" for="password">Password</label>
       <input type="password" class="p-2" id="password" v-model="password" placeholder="Password" />
-
       <br />
       <button type="submit" class="btn btn-primary" v-on:click.prevent="login">Submit</button>
     </form>
@@ -24,7 +16,6 @@
 <script>
 import { httpClient } from "@/services/Api.js";
 import moment from "moment";
-
 export default {
   name: "login",
   props: {
@@ -34,8 +25,7 @@ export default {
     return {
       email: undefined,
       password: undefined,
-      emailMessage: "",
-      passwordMessage: ""
+      firstTime: undefined
     };
   },
   methods: {
@@ -44,87 +34,65 @@ export default {
         email: this.email,
         password: this.password
       };
-
-      var e = this.isEmailValid();
-      var p = this.isPasswordValid();
-
-      if(e && p) {
-        httpClient
-          .post("/auth/login", user)
-          .then(response => {
-            localStorage.setItem("User-token", response.data.accessToken);
-            localStorage.setItem("Refresh-token", response.data.refreshToken);
-            localStorage.setItem("Email", response.data.email);
-            localStorage.setItem("Authority", response.data.authority);
-
-            let time = moment().add(response.data.expiresIn);
-            localStorage.setItem("Expiary", time);
-
-            let role = response.data.authority;
-            let path;
-            if (role === "ROLE_CCADMIN") {
-              path = "/ccadmin";
-            } else if (role === "ROLE_CADMIN") {
-              path = "/cadmin/profile";
-            } else if (role === "ROLE_DOCTOR") {
-              path = "/doc/profile";
-            } else if (role === "ROLE_NURSE") {
-              path = "/nurse/profile";
-            } else if (role === "ROLE_PATIENT") {
-              path = "/patient/profile";
-            } else if (role === "ROLE_REQUEST") {
-              path = "/login";
-              this.$vToastify.info({
-                body: "You have to be accepted by admin to log in." ,
-                title: "Success",
-                type: "success",
-                canTimeout: true,
-                append: false, duration: 2000
-              });
-            }
-            this.$router.push(path);
-          })
-          .catch(error => {
-            alert(error);
+      httpClient
+        .post("/auth/login", user)
+        .then(response => {
+          localStorage.setItem("User-token", response.data.accessToken);
+          localStorage.setItem("Refresh-token", response.data.refreshToken);
+          localStorage.setItem("Email", response.data.email);
+          localStorage.setItem("Authority", response.data.authority);
+          let time = moment().add(response.data.expiresIn);
+          localStorage.setItem("Expiary", time);
+          let role = response.data.authority;
+          let path;
+          httpClient
+            .get("/users/" + response.data.email)
+            .then(response => {
+              this.response = response;
+              this.firstTime = this.response.data;
+              if (this.firstTime === true && role != "ROLE_PATIENT") {
+                this.$router.push("/changePassword");
+              } else {
+                if (role === "ROLE_CCADMIN") {
+                  path = "/ccadmin";
+                } else if (role === "ROLE_CADMIN") {
+                  path = "/cadmin/profile";
+                } else if (role === "ROLE_DOCTOR") {
+                  path = "/doc/profile";
+                } else if (role === "ROLE_NURSE") {
+                  path = "/nurse/profile";
+                } else if (role === "ROLE_PATIENT") {
+                  path = "/patient/profile";
+                } else if (role === "ROLE_REQUEST") {
+                  path = "/login";
+                  this.$vToastify.info({
+                    body: "You hove to be accepted by admin to log in.",
+                    title: "Success",
+                    type: "success",
+                    canTimeout: true,
+                    append: false,
+                    duration: 2000
+                  });
+                }
+              }
+              this.$router.push(path);
+              //}
+            })
+            .catch(error => {
+              this.error = error;
+            });
+        })
+        .catch(() => {
+          this.$vToastify.info({
+            body: "Your username or passwrord are not correct.",
+            title: "Warning",
+            type: "warning",
+            canTimeout: true,
+            append: false,
+            duration: 2000
           });
-      }
-    },
-    isEmailValid: function() {
-      let emailInput = document.getElementById("email");
-
-      if(!this.email) { // email is either empty string, undefined or null
-          this.emailMessage = "E-mail is required";
-          emailInput.style.border = '2px solid red';
-          return false;
-      } else if (!this.email.includes("@")) {
-          this.emailMessage = "Invalid e-mail format";
-          emailInput.style.border = '2px solid red';
-          return false;
-      } else {
-          this.emailMessage = "";
-          emailInput.style.border = null;
-          return true;
-      }
-    },
-    isPasswordValid: function() {
-      let passwordInput = document.getElementById("password");
-
-      if(!this.password){
-          this.passwordMessage = "Password is required";
-          passwordInput.style.border = '2px solid red';
-          return false;
-      } else {
-          this.passwordMessage = "";
-          passwordInput.style.border = null;
-          return true;
-      }
+        });
     }
   }
 };
 </script>
-
-<style scoped>
-#err {
-  color: red;
-}
-</style>
