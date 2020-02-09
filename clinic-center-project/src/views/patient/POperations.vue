0031777e5a36a91c41a4d49cd36bf4f5f595efed
meelosch <57445151+meelosch@users.noreply.github.com>
2020-02-09 23:07:57 +0100

    front-end: list of patient's operations

--- clinic-center-project/src/views/patient/POperations.vue
+++ clinic-center-project/src/views/patient/POperations.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="d-flex p-2 justify-content-center">
-    <h1>History of operations</h1>
+  <div class="d-flex flex-column p-2 justify-content-center">
+    <div align="center">
+      <h1>History of operations</h1>
+    </div>
+    <br>
+    <div class="d-flex flex-row flex-wrap">
+        <div
+          class="card border-primary"
+          v-for="o in operations" v-bind:key="o.id"
+          style="max-width: 20rem; max-height: 18rem; float: left; margin: 10px"
+        >
+          <div class="card-body">
+            <h4 class="card-title">Date: {{o.date}}</h4>
+            <p class="card-text">Clinic: {{o.operationRoom.clinic.name}}</p>
+            <p class="card-text">Operation room: {{o.operationRoom.name}}</p>
+          </div>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
+import { httpClient } from "@/services/Api.js";
+import _ from "lodash";
+
 export default {
   data: function() {
-    return {};
+    return {
+      operations: []
+    };
+  },
+  mounted() {
+      var email = localStorage.getItem("Email");
+
+      httpClient
+        .get("/operation/patient/" + email)
+        .then(response => {
+        this.operations = _.cloneDeep(response.data);
+      })
+      .catch(error => {
+        alert(error);
+      });
   }
 };
 </script>
