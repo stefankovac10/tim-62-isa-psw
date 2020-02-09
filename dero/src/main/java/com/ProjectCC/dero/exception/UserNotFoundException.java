2d0087cfcbe450e7490947b6050965627697f4cb
stefankovac10 <stefankovac42@gmail.com>
2020-02-09 19:33:55 +0100

    Set up for test

--- dero/src/main/java/com/ProjectCC/dero/exception/UserNotFoundException.java
+++ dero/src/main/java/com/ProjectCC/dero/exception/UserNotFoundException.java
@@ -1,11 +1,5 @@
 package com.ProjectCC.dero.exception;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String errorMessage, Throwable error) {
-        super(errorMessage, error);
-    }
 
-    public UserNotFoundException(String errorMessage) {
-        super(errorMessage);
-    }
 }
