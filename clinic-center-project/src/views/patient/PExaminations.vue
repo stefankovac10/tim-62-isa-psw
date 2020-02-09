245ab263b44b71c16f46d7be2d6d0d7fe72a1162
meelosch <57445151+meelosch@users.noreply.github.com>
2020-02-09 22:00:53 +0100

    Revert "Revert "Revert "method for searching patient's examinations on service"""
    This reverts commit 18d33b09ff70071d3afe7264b46a62ed98a8700c.

--- clinic-center-project/src/views/patient/PExaminations.vue
+++ clinic-center-project/src/views/patient/PExaminations.vue
@@ -1,47 +1,13 @@
 <template>
-  <div class="d-flex flex-column p-2 justify-content-center">
-    <div align="center">
-      <h1>History of examinations</h1>
-    </div>
-    <br>
-    <div class="d-flex flex-row flex-wrap">
-        <div
-          class="card border-primary"
-          v-for="e in examinations" v-bind:key="e.id"
-          style="max-width: 20rem; max-height: 18rem; float: left; margin: 10px"
-        >
-          <div class="card-body">
-            <h4 class="card-title">Date: {{e.date}}</h4>
-            <p class="card-text">Doctor: {{e.doctor.firstName + " " + e.doctor.lastName}}</p>
-            <p class="card-text">Descripion: {{e.report}}</p>
-          </div>
-        </div>
-      </div>
+  <div class="d-flex p-2 justify-content-center">
+    <h1>History of examinations</h1>
   </div>
-  
 </template>
 
 <script>
-import { httpClient } from "@/services/Api.js";
-import _ from "lodash";
-
 export default {
   data: function() {
-    return {
-      examinations: []
-    };
-  },
-  mounted() {
-      var email = localStorage.getItem("Email");
-
-      httpClient
-        .get("/examination/patient/" + email)
-        .then(response => {
-        this.examinations = _.cloneDeep(response.data);
-      })
-      .catch(error => {
-        alert(error);
-      });
+    return {};
   }
 };
 </script>
